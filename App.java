7803253129379a7687c2b85d841eca979e635d57
Iván Morales <48416564+ivansmorales@users.noreply.github.com>
2019-11-10 23:31:57 -0600

    GUI

---
+++ App.java
@@ -0,0 +1,147 @@
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+import java.awt.event.*;
+import java.util.Scanner;
+import java.lang.*;
+import java.util.List;
+import java.util.ArrayList;
+
+public class App extends JFrame {
+
+	/* Variables para Gui */
+	private JButton loadBtn = new JButton("Load File");
+	private JButton verifyBtn = new JButton("Verify");
+	private JTextField txtA = new JTextField();
+	private JTextField txtB = new JTextField();
+	private JLabel lblA = new JLabel("Load your file, please.");
+	private JLabel lblB = new JLabel("Enter a string: ");
+	private JLabel lblC = new JLabel("Result: ");
+
+	/* Variables para App */
+	static ArrayList<String> nonTerminalSymbols;
+	static ArrayList<String> terminalSymbols;
+	static String startingSymbol;
+	static ArrayList<ArrayList<ArrayList<String>>> productionsGrammar;
+
+
+	public App() {
+		setTitle("IP.2");
+		setSize(400, 300);
+		setLocation(new Point(300, 200));
+		setLayout(null);
+		setResizable(false);
+		initComponent();
+		initEvent();
+		setVisible(true);
+	}
+
+	private void initComponent() {
+		lblA.setBounds(100, 10, 200, 20);
+		lblA.setFont(new Font("Arial", Font.BOLD, 18));
+		loadBtn.setBounds(100, 50, 200, 30);
+		loadBtn.setFont(new Font("Arial", Font.BOLD, 16));
+		lblB.setBounds(100, 90, 200, 20);
+		lblB.setFont(new Font("Arial", Font.PLAIN, 16));
+		txtA.setBounds(100, 120, 200, 25);
+		txtA.setFont(new Font("Arial", Font.PLAIN, 16));
+		verifyBtn.setBounds(100, 150, 200, 30);
+		verifyBtn.setFont(new Font("Arial", Font.BOLD, 16));
+		lblC.setBounds(100, 200, 200, 20);
+		lblC.setFont(new Font("Arial", Font.PLAIN, 16));
+		txtB.setBounds(100, 230, 200, 25);
+		txtB.setFont(new Font("Arial", Font.PLAIN, 16));
+
+		add(lblA);
+		add(loadBtn);
+		add(lblB);
+		add(txtA);
+		add(verifyBtn);
+		add(lblC);
+		add(txtB);
+	}
+
+	private void initEvent() {
+
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(1);
+			}
+		});
+
+		loadBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					loadBtn(e);
+				} catch (Exception exception) {
+					System.out.println("Exception");
+				}
+			}
+		});
+
+		verifyBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				verifyBtn(e);
+			}
+		});
+	}
+
+	private void loadBtn(ActionEvent evt) {
+		Scanner sc = new Scanner(System.in);
+		JFileChooser jf = new JFileChooser();
+		
+		String aux2 = "";
+
+		int aux = jf.showOpenDialog(null);
+		if (aux == JFileChooser.APPROVE_OPTION) {
+			try {
+				File ff = jf.getSelectedFile();
+				//System.out.println(ff);
+				System.out.println(ff.getName());
+				/*
+				* Lo que yo buscaba era una forma de usar Scanner con JFileChooser para reutilizar
+				* todo el código del IP.1 Entonces con .getName(), un metodo de FileReader
+				* obtenemos el nombre del archivo a utilizar y asi se lo pasamos a Scanner
+				*/
+				String noTerminales = "";
+				Scanner s = new Scanner(new File(ff.getName()));
+				/* Hay que comprobar que si fue leido*/
+				noTerminales = s.nextLine();
+				nonTerminalSymbols = new ArrayList<String>();
+				String[] splitStr = noTerminales.split(",");
+				for (int i = 0 ; i < splitStr.length ; i++) {
+					nonTerminalSymbols.add(splitStr[i]);
+					System.out.println(nonTerminalSymbols.get(i));
+				}
+				s.close();
+
+				// txtB.read(fff, "");
+			} catch (Exception ex) {
+				// Logger.getLogger(FilesDemo.class.getName()).log(Level.SEVERE, null, ex);
+			}
+		}
+	}
+
+/* 	public static ArrayList<String> getNonTerminalSymbols(String archivo){
+		String estados = "";
+		try {
+		  Scanner s = new Scanner(new File(archivo));
+		  estados = s.nextLine();
+		  states = new ArrayList<String>();
+		  String[] splitStr = estados.split(",");
+		  for (int i = 0 ; i < splitStr.length ; i++) {
+			states.add(splitStr[i]);
+		  }
+		  s.close();
+		} catch (FileNotFoundException e) {
+		  System.out.println("File not found.");
+		}
+		return states;
+	}  */
+
+
+	private void verifyBtn(ActionEvent evt) {
+		System.out.println("Apretado");
+	}
+
+}
--- Main.java
+++ Main.java
@@ -1,52 +1,6 @@
-/**
- * Integrative practice Part 2
- * Computational mathematics
- * @authors: Anne Constanze Schreiber          A01650066
- *           Jorge Ivan Morales González       A01652650
- * This program computes a grammar and sees if it accepts the string, using tree derivations.
- */
+public class Main {
+	public static void main(String args[]) {
+		new App();
 
-import java.io.*;
-import java.util.Scanner;
-import javax.swing.*;
-import java.util.List;
-import java.util.ArrayList;
-
-class Main {
-    
-    static ArrayList<String> nonTerminalSymbols;
-    static ArrayList<String> terminalSymbols;
-    static String startingSymbol;
-    static ArrayList<ArrayList<ArrayList<String>>> productionsGrammar;
-
-    public static void main(String [] args) throws Exception{
-        readFile();
-    }
-
-    public static void readFile() throws Exception {
-        JFileChooser chooser = new JFileChooser();
-        int filename = chooser.showOpenDialog(null);
-        if (filename == JFileChooser.APPROVE_OPTION){
-            File f = chooser.getSelectedFile();
-            Scanner scan = new Scanner (new FileReader(f));
-        }
-    }
-
-    public static ArrayList<String> nonTerminalSymbols(String archivo){
-        String estados = "";
-        try {
-          Scanner s = new Scanner(new File(archivo));
-          estados = s.nextLine();
-          states = new ArrayList<String>();
-          String[] splitStr = estados.split(",");
-          for (int i = 0 ; i < splitStr.length ; i++) {
-            states.add(splitStr[i]);
-          }
-          s.close();
-        } catch (FileNotFoundException e) {
-          System.out.println("File not found.");
-        }
-        return states;
-      } 
-
+	}
 }
